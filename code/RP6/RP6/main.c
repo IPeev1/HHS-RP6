bfe84b44f234f511ade97197fcefdb9d1b79bac1
Rens van den Heuvel <rensvdheuvel@hotmail.com>
2018-03-12 11:12:21 +0100

    Initial Commit

---
+++ code/RP6/RP6/main.c
@@ -0,0 +1,23 @@
+/*
+ * RP6.c
+ *
+ * Created: 12-Mar-18 11:07:57
+ * Author : Rens
+ *			Sander
+ *			Ivan
+ *			Willem
+ *			Matthijs
+ *			
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+

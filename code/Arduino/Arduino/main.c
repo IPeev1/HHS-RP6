4932c285aa96383c31b172d468712707bce12831
Rens van den Heuvel <rensvdheuvel@hotmail.com>
2018-03-12 11:16:17 +0100

    add: arduino project file

---
+++ code/Arduino/Arduino/main.c
@@ -0,0 +1,18 @@
+/*
+ * Arduino.c
+ *
+ * Created: 12-Mar-18 11:15:51
+ * Author : Rens
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+

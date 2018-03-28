82765a8b9c16139ca56495387e5503348e80b847
MatthijsHeidema <17117100@student.hhs.nl>
2018-03-28 15:55:41 +0200

    new: dev: First draft for bumper functions

---
+++ code/RP6/RP6/bumpers.c
@@ -0,0 +1,68 @@
+/*
+ * bumpers.c
+ *
+ * Created: 3/28/2018 3:10:43 PM
+ *  Author: mc_he
+ */ 
+#include <avr/io.h>
+#include <util/delay.h>
+#include "bumpers.h"
+
+#define PORT_L PORTB
+#define PIN_L PINB
+#define DDR_L DDRB
+#define IO_L PINB0
+
+#define PORT_R PORTC
+#define PIN_R PINC
+#define DDR_R DDRC
+#define IO_R PINC6
+
+#define DISTANCE 0
+
+int getBumperLeft() {
+	
+	PORT_L &= ~(1 << IO_L);
+	DDR_L &= ~(1 << IO_L);
+	_delay_us(2);
+	
+	uint8_t pushed = PIN_L & (1 << IO_L);
+	
+	DDR_L |= (1 << IO_L);
+	PORT_L |= (1 <<  IO_L);
+	
+	return pushed;
+}
+
+int getBumperRight() {
+	
+	PORT_R &= ~(1 << IO_R);
+	DDR_R &= ~(1 << IO_R);
+	_delay_us(2);
+	
+	uint8_t pushed = PIN_R & (1 << IO_R);
+	
+	DDR_R |= (1 << IO_R);
+	PORT_R |= (1 <<  IO_R);
+	
+	return pushed;
+}
+
+int getBumpers() {
+	
+	return getBumperLeft() || getBumperRight();
+}
+
+void bumperCheck() {
+	
+	if (getBumpers()) {
+		long distance = DISTANCE;
+	
+		while (DISTANCE > distance - 5) {
+		
+			//drive backwards
+		}
+	
+		//throttle = 0
+	}
+}
---
+++ code/RP6/RP6/bumpers.h
@@ -0,0 +1,17 @@
+/*
+ * bumpers.h
+ *
+ * Created: 3/28/2018 3:10:58 PM
+ *  Author: mc_he
+ */ 
+
+
+#ifndef BUMPERS_H_
+#define BUMPERS_H_
+
+int getBumperLeft();
+int getBumperRight();
+int getBumpers();
+
+
+#endif /* BUMPERS_H_ */

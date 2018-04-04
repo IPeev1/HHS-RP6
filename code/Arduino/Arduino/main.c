41c3f42ce1d663826b1aefa7d99dcaae0c5af1c1
Rens van den Heuvel <rensvdheuvel@hotmail.com>
2018-04-04 15:11:11 +0200

    add: correct main for compass code

--- code/Arduino/Arduino/main.c
+++ code/Arduino/Arduino/main.c
@@ -28,6 +28,7 @@ uint32_t ultrasonicSensorTimer = 0;
 uint32_t ultrasonicSensorSpeed = 250000;
 uint32_t stoptimer = 0;
 uint32_t stoptimerspeed = 100000;
+int compassFlag = 0;
 
 //Micros function ---------------------------------
 uint64_t micros();								//Keep track of the amount of microseconds passed since boot
@@ -53,6 +54,7 @@ void TWIwaitUntilReady();
 void checkCode(uint8_t code);
 void writeToSlave(uint8_t address, uint8_t dataByte[]);
 void readFromSlave(uint8_t address);
+void readFromCompass();
 //-------------------------------------------------
 #define DATASIZE 20										//Define how much data is transferred per transmit (Array length)
 #define RP6_ADDRESS 3									//Define the slave address we are talking to, can currently be only one
@@ -84,7 +86,7 @@ struct rp6DataBP {
 	uint16_t	driveSpeedThreshold;	//Minimal power needed to actually start moving	(Default: 5000)
 	uint32_t	updateSpeed;			//Interval time between updates					(Default: 200000)
 	uint8_t		enableBeeper;			//Set to 1 to enable reverse driving beeper		(Default: 1	(On))
-	uint16_t	compassAngle;			//Degrees from north given by compass
+	uint64_t	compassAngle;			//Degrees from north given by compass
 } rp6Data;
 
 
@@ -210,7 +212,6 @@ int main(void){
 	
 	while (1){
 		if (ultrasonicSensorTimer < micros()) {
-			//printUltrasonicSensorDistance();
 			writeString("\f\r");
 			writeString("Distance to object: ");
 			writeInt(ultrasonicSensor());
@@ -223,6 +224,8 @@ int main(void){
 			if(rp6Data.turnDirection == -1) writeString("turning left");
 			else if(rp6Data.turnDirection == 0) writeString("going straight");
 			else if(rp6Data.turnDirection == 1) writeString("turning right");
+			writeString("\n\rCompass Angle: ");
+			writeInt(rp6Data.compassAngle);
 			
 			ultrasonicSensorTimer = micros() + ultrasonicSensorSpeed;
 		}
@@ -311,9 +314,13 @@ ISR(TWI_vect){
 	
 	switch(TWSR){
 		case 0x40:
-		clearReceiveData();
-		bytecounter = 0;
-		TWISendACK();
+		if(compassFlag){
+			TWISendNACK();
+		}else{
+			clearReceiveData();
+			bytecounter = 0;
+			TWISendACK();
+		}
 		break;
 		
 		case 0x50:
@@ -328,9 +335,16 @@ ISR(TWI_vect){
 		break;
 		
 		case 0x58:
-		receiveDataTWI[bytecounter] = TWDR;
-		TWISendStop();
-		I2C_receiveInterpreter();
+		if(!compassFlag){
+			receiveDataTWI[bytecounter] = TWDR;
+			TWISendStop();
+			I2C_receiveInterpreter();
+		}else{
+			uint64_t temp = TWDR;
+			rp6Data.compassAngle = ((temp * 360) / 255);
+			TWISendStop();
+			compassFlag = 0;
+		}
 		break;
 	}
 }
@@ -339,9 +353,11 @@ ISR(TWI_vect){
 ISR(TIMER2_OVF_vect){
 	static int counter = 0;
 	
-	if(counter == 6){
+	if(counter == 4){
 		rp6DataConstructor();
-		}else if(counter >= 12){
+	}else if(counter == 8){
+		readFromCompass();
+	}else if(counter >= 12){
 		readFromSlave(RP6_ADDRESS);
 		counter = 0;
 	}
@@ -460,6 +476,28 @@ void writeToSlave(uint8_t address, uint8_t dataByte[]){
 	
 	TWISendStop();
 	
+}
+
+void readFromCompass(){
+	compassFlag = 1;
+	TWISendStart();
+	TWIwaitUntilReady();
+	checkCode(0x08);
+	
+	TWIWrite(0xC0);
+	TWIwaitUntilReady();
+	checkCode(0x18);
+	
+	TWIWrite(1);
+	TWIwaitUntilReady();
+	checkCode(0x28);
+	
+	TWISendStart();
+	TWIwaitUntilReady();
+	checkCode(0x10);
+	
+	TWIWrite(0xC1);
+	TWIwaitUntilReady();
 }
 
 
